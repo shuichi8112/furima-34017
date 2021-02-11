ba094e990a2542ce650ffc12f30148150acc137d
shuichi8112 <shuichi8112@gmail.com>
2021-02-11 12:28:44 +0900

    ログイン・アウト機能実装、エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+
+  validates :nickname,        presence: true
+
+  with_options presence: true, format: { with: /[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /[ア-ン゛゜ァ-ォャ-ョー]/, message: 'Full-width katakana characters' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birth,           presence: true
 end
