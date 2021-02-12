a491865e72d5023957acb540964fcee460cb2620
shuichi8112 <shuichi8112@gmail.com>
2021-02-12 16:38:49 +0900

    指摘箇所修正1回目　　ユーザー管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,14 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
 
-  validates :nickname,        presence: true
-
-  validates :email, presence: true, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
-
-  validates :last_name, presence: true, format: { with: /[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
-  validates :first_name, presence: true, format: { with: /[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
-
-  validates :last_name_kana, presence: true, format: { with: /[ア-ン゛゜ァ-ォャ-ョー]/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, presence: true, format: { with: /[ア-ン゛゜ァ-ォャ-ョー]/, message: 'Full-width katakana characters' }
-
-  validates :birth,           presence: true
-
-  validates :password, presence: true,
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+    validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :last_name, format: { with: /[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
+    validates :first_name, format: { with: /[ぁ-んァ-ヶ一-龥々]/, message: 'Full-width characters' }
+    validates :last_name_kana, format: { with: /[ア-ン゛゜ァ-ォャ-ョー]/, message: 'Full-width katakana characters' }
+    validates :first_name_kana, format: { with: /[ア-ン゛゜ァ-ォャ-ョー]/, message: 'Full-width katakana characters' }
+    validates :birth
+  end
 end
