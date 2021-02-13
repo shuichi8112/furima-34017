146ac3d8d1e08f27008746d42e2c626706d56190
shuichi8112 <shuichi8112@gmail.com>
2021-02-13 23:35:05 +0900

    画像投稿機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,10 @@ class ItemsController < ApplicationController
   def new
     @items = Item.all
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
