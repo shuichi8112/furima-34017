7c682b18b7751e99b173073c9b95f75db9facdc4
shuichi8112 <shuichi8112@gmail.com>
2021-02-14 01:00:59 +0900

    ActiveaHashとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,25 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :order
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[-]?[0-9]+(\.[0-9]+)?\z/ }
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :shipping_day_id
+    end
+  end
 end
