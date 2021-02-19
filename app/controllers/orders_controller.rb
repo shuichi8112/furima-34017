d3542e5cc43fe47fe0f451cfc073a5f501c05929
shuichi8112 <shuichi8112@gmail.com>
2021-02-19 20:49:46 +0900

    クレジットカード機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+       pay_item
        @order_address.save
        redirect_to root_path
     else
@@ -16,8 +17,16 @@ class OrdersController < ApplicationController
   end  
 
   private
-
   def order_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class OrderAddress
     validates :phone_number, format: {with: /\A\d{11}\z/, message: "Input only number"}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
